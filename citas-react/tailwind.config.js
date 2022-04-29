683b4262c5fc47ffacce3fe0dbac28f425b69d13
CARLOS IGNACIO RIOS MADRID <15171192@itculiacan.edu.mx>
2022-04-29 15:22:04 -0600

    iniciando proyecto de control de gastos

--- citas-react/tailwind.config.js
+++ citas-react/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "index.html",
+    "./src/**/*.jsx",
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
