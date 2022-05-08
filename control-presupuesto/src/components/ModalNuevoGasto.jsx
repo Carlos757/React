4d43bb508782ab3de6bc75eecc6865182f58f8ad
CARLOS IGNACIO RIOS MADRID <15171192@itculiacan.edu.mx>
2022-05-07 20:30:34 -0600

    componente modalNuevoGasto funcionando

--- control-presupuesto/src/components/ModalNuevoGasto.jsx
+++ control-presupuesto/src/components/ModalNuevoGasto.jsx
@@ -1,7 +1,124 @@
-import React from "react";
+import { useState, useEffect } from "react";
+const url = "https://60cdf0b491cc8e00178dc287.mockapi.io/";
 
-const ModalNuevoGasto = () => {
-    return <div>ModalNuevoGasto</div>;
+const ModalNuevoGasto = ({
+    setModalNuevoGasto,
+    categorias,
+    gastos,
+    setGastos,
+    disponible,
+}) => {
+    const [nombreGasto, setNombreGasto] = useState("");
+    const [cantidad, setCantidad] = useState("");
+    const [categoria, setCategoria] = useState(0);
+    const [error, seterror] = useState(false);
+
+    const handleClickOutside = () => {
+        setModalNuevoGasto(false);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (validaFormulario()) {
+            setGastos([
+                ...gastos,
+                {
+                    id: generateId(),
+                    nombre: nombreGasto,
+                    cantidad: parseInt(cantidad),
+                    categoria: categoria,
+                },
+            ]);
+            setModalNuevoGasto(false);
+        }
+    };
+    function validaFormulario() {
+        const cantidadParseada = parseInt(cantidad);
+
+        if (!cantidadParseada || cantidadParseada < 0) {
+            seterror(true);
+            return false;
+        }
+        if (nombreGasto === "" || cantidad === "" || categoria === 0) {
+            seterror(true);
+            return false;
+        }
+        if (cantidadParseada > disponible) {
+            seterror(true);
+            return false;
+        }
+        seterror(false);
+        return true;
+    }
+    function generateId() {
+        const date = Date.now().toString(36);
+        const random = Math.random().toString(36).substring(2);
+        return random + date;
+    }
+    return (
+        <div className="d-flex justify-center align-center">
+            <div className="modal-backdrop" onClick={handleClickOutside}></div>
+            <div className="modal">
+                <form
+                    className="card-modal form-modal"
+                    onSubmit={(e) => handleSubmit(e)}
+                >
+                    <p className="card-title">Agrega tu gasto</p>
+                    <hr />
+                    <div className="form-modal-content">
+                        <label className="etiqueta" htmlFor="nombre-gasto">
+                            Nombre de Gasto
+                        </label>
+                        <input
+                            required
+                            type="text"
+                            name="nombre-gasto"
+                            id="nombre-gasto"
+                            value={nombreGasto}
+                            onChange={(e) => setNombreGasto(e.target.value)}
+                            placeholder="Ingrese nombre"
+                        />
+                        <label className="etiqueta" htmlFor="cantidad">
+                            Cantidad
+                        </label>
+                        <input
+                            required
+                            type="number"
+                            name="cantidad"
+                            id="cantidad"
+                            value={cantidad}
+                            onChange={(e) => setCantidad(e.target.value)}
+                            placeholder="Ingrese cantidad"
+                        />
+                        <label className="etiqueta" htmlFor="categoria">
+                            Categoría
+                        </label>
+                        <select
+                            required
+                            value={categoria}
+                            name="categoria"
+                            id="categoria"
+                            onChange={(e) => setCategoria(e.target.value)}
+                        >
+                            <option hidden>Selecciona una Categoría</option>
+                            {categorias.length > 0 &&
+                                categorias.map((cat) => (
+                                    <option key={cat.id} value={cat.id}>
+                                        {cat.nombre}
+                                    </option>
+                                ))}
+                        </select>
+
+                        <button
+                            type="submit"
+                            className="btn btn-primary btn-agregar"
+                        >
+                            Agregar
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default ModalNuevoGasto;
--- control-presupuesto/src/components/Presupuesto.jsx
+++ control-presupuesto/src/components/Presupuesto.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import imgNuevoGasto from "./../img/nuevo-gasto.svg";
 import ModalNuevoGasto from "./ModalNuevoGasto";
+const url = "https://60cdf0b491cc8e00178dc287.mockapi.io/";
 
 const Presupuesto = ({
     presupuesto,
     setPresupuesto,
     setMuestraPresupuesto,
 }) => {
+    const [categorias, setCategorias] = useState([]);
+    const [gastos, setGastos] = useState([]);
+
     const [gastado, setGastado] = useState(
         parseFloat(JSON.parse(localStorage.getItem("gastado")) ?? 0)
     );
@@ -15,19 +19,48 @@ const Presupuesto = ({
     const [modalNuevoGasto, setModalNuevoGasto] = useState(false);
 
     useEffect(() => {
-        return obtenerPorcentajeGastado();
+        cargaCategorias();
     }, []);
 
+    useEffect(() => {
+        calcularGastado();
+    }, [gastos]);
+
+    useEffect(() => {
+        setPorcentajeGastado(((gastado * 100) / presupuesto).toFixed(2));
+        setDisponible(presupuesto - gastado);
+    }, [gastado]);
+
     const handleReset = () => {
-        setPresupuesto("");
-        setMuestraPresupuesto(false);
+        if (
+            window.confirm(
+                "¿Estás seguro de que quieres resetear el presupuesto?"
+            )
+        ) {
+            setPresupuesto("");
+            setMuestraPresupuesto(false);
+        }
     };
     const handleNuevoGasto = () => {
         setModalNuevoGasto(true);
     };
 
-    function obtenerPorcentajeGastado() {
-        setPorcentajeGastado(((gastado * 100) / presupuesto).toFixed(2));
+    async function cargaCategorias() {
+        const response = await fetch(url + "categorias");
+        const result = await response.json();
+        if (result) {
+            setCategorias(result);
+        }
+    }
+
+    function calcularGastado() {
+        if (gastos.length > 0) {
+            let sumaGastos = gastos.reduce((suma, gasto) => {
+                return suma + gasto.cantidad;
+            }, 0);
+            console.log(sumaGastos);
+            setGastado(sumaGastos);
+        }
     }
     return (
         <div className="card">
@@ -36,7 +69,7 @@ const Presupuesto = ({
                     <progress
                         className="progress-bar"
                         max={presupuesto}
-                        value={porcentajeGastado}
+                        value={gastado}
                     />
                     <span className="etiqueta txt-center">
                         {porcentajeGastado}% gastado
@@ -63,7 +96,16 @@ const Presupuesto = ({
             <a className="btn-nuevo-gasto" onClick={handleNuevoGasto}>
                 <img src={imgNuevoGasto} alt="nuevo gasto" />
             </a>
-            {modalNuevoGasto && <ModalNuevoGasto />}
+            {modalNuevoGasto && (
+                <ModalNuevoGasto
+                    setModalNuevoGasto={setModalNuevoGasto}
+                    categorias={categorias}
+                    setCategorias={setCategorias}
+                    gastos={gastos}
+                    setGastos={setGastos}
+                    disponible={disponible}
+                />
+            )}
         </div>
     );
 };
